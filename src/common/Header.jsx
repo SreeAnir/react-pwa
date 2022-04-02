bff7670cbbb53ce7bdb7935eb7005be99befbecf
sreekala <sreekala@gmail.com>
2022-04-03 01:39:20 +0530

    link , react dom version downgraded

---
+++ src/common/Header.jsx
@@ -0,0 +1,49 @@
+import React ,{Component} from 'react';
+import Nav from './Nav';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Home from '../components/Home';
+import Register from '../components/Register';
+import Login from '../components/Login';
+
+export class Header extends Component{
+    render(){
+        return ( 
+            <Router>
+            <div>
+            <Nav/> 
+
+           
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link  to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        
+        <Switch>
+        <Route exact path="/about" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+
+        </Switch>
+      </div>
+    </Router>
+
+        )
+    }
+}
+
+export default Header 
---
+++ src/components/Forgot.jsx
@@ -0,0 +1,11 @@
+import React ,{Component} from 'react';
+ 
+export class Forgot extends Component{
+    render(){
+        return (<div>
+             <h1>Forgot</h1>
+        </div>)
+    }
+}
+
+export default Forgot ; 
